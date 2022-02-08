8a760a2a3ffe2848ce273370405e2a0fd8725d3e
George Knap <g.knap@solar-log.com>
2022-02-08 15:51:20 +0100

    test: sync jest config with jest-preset-angular

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,7 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { paths } = require('./tsconfig.json').compilerOptions;
+const { paths } = require("./tsconfig.spec.json").compilerOptions;
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "jest-preset-angular",
-  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: "<rootDir>" }),
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-      stringifyContentPathRegex: "\\.(html|svg)$",
-      isolatedModules: true,
-    },
-  },
 };
